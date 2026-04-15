29184396b008a5be3b0ac69b4fcaac6cd6b4cc7d
Your Name <you@example.com>
2026-04-15 10:03:21 -0300

    - al levantar el servidor muestra links al root y al healthcheck por consola, - el healthcheck envia uptime del servidor y un timestamp en formato json

--- index.js
+++ index.js
@@ -8,9 +8,17 @@ app.use(express.json());
 const puerto = 3000;
 
 app.get("/health", (req, res) => {
-    res.sendStatus(200);
-})
+    res.status(200).json({
+        status: "ok",
+        timestamp: new Date().toISOString(),
+        uptime: process.uptime(),
+    });
+});
 
-app.listen(puerto , () => {
-    console.log("Servidor conectado en puerto: " + puerto);
-})
+app.listen(puerto, () => {
+    const urlBase = `http://localhost:${puerto}`;
+    console.log(`Servidor conectado en:\t${urlBase}`);
+    console.log(`Health check en:\t${urlBase}/health`);
+});
+
+
