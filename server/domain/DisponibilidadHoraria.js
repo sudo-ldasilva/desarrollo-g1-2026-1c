4a8240ccfb635f7408417e1957bcb001edb4f0d7
ValentinOcchiuzzi <vocchiuzzi@frba.utn.edu.ar>
2026-04-26 17:39:02 -0300

    Agrego duracion en mins a una disponibilidad horaria.

--- server/domain/DisponibilidadHoraria.js
+++ server/domain/DisponibilidadHoraria.js
@@ -5,6 +5,7 @@ class DisponibilidadHoraria {
         this.diaSemana = diaSemana;
         this.horaDesde = horaDesde;
         this.horaHasta = horaHasta;
+        this.duracionEnMins = duracionEnMinutos(horaDesde, horaHasta);
     }
 
     get diaSemana() { return this._diaSemana; }
@@ -14,6 +15,23 @@ class DisponibilidadHoraria {
     set diaSemana(dia) { this._diaSemana = dia; }
     set horaDesde(horaDesde) { this._horaDesde = horaDesde; }
     set horaHasta(horaHasta) { this._horaHasta = horaHasta; }
+
+}
+
+function aMinutos(hora) {
+    const [h, m] = hora.split(":").map(Number);
+    return h * 60 + m;
+}
+
+function duracionEnMinutos(desde, hasta) {
+    const minDesde = aMinutos(desde);
+    const minHasta = aMinutos(hasta);
+
+    if (minHasta < minDesde) {
+        return (24 * 60 - minDesde) + minHasta;
+    }
+
+    return minHasta - minDesde;
 }
 
 export default DisponibilidadHoraria;
