e90d68384b5b077df671da000d99ce30589cc6f4
ValentinOcchiuzzi <vocchiuzzi@frba.utn.edu.ar>
2026-05-07 09:48:34 -0300

    Agregego los app.use de los middlewares

--- server/app.js
+++ server/app.js
@@ -2,12 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 
 import router from "./routes/router.js";
+import { notFoundHandler } from "./middlewares/notFoundHandler.js";
+import { errorLogger } from "./middlewares/errorLogger.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 dotenv.config();
 
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
 app.use(router);
+
+app.use(notFoundHandler);
+app.use(errorLogger);
+app.use(errorHandler);
 
 export default app;
