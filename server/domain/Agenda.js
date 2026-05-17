a6259bd45095c2c764f0cf74cccc674ae5e8542b
sudo-ldasilva <205469926+sudo-ldasilva@users.noreply.github.com>
2026-05-17 19:49:08 -0300

    Elimina versión anterior de generarTurnos()

--- server/domain/Agenda.js
+++ server/domain/Agenda.js
@@ -6,7 +6,7 @@ export class Agenda {
     //recorre rango de fechas
     //si el dia de la fecha concuerda con disponibilidad
     //divide la disponibilidad
-    //instancia 
+    //instancia
     generarTurnosPara(medico, fechaDesde, fechaHasta) {
 
         const turnos = [];
@@ -58,35 +58,3 @@ export class Agenda {
         // TODO Return Turnos[] que hace esto?
     }
 }
-
-
-// generarTurnosPara(objetivo, medico) {
-//         let puedeRealizarlo = objetivo.puedeRealizarlo(medico);
-//         if (!puedeRealizarlo.puede) {
-//             throw new Error(puedeRealizarlo.msg);
-//         }
-
-//         if (!medico.tieneAlgunaSede()) {
-//             throw new Error(`Médico ${medico.nombre} no tiene sedes asignadas`);
-//         }
-
-//         const turnos = [];
-
-//         const now = new Date(Date.now());
-//         const nombreDeDias = [DiaSemana.DOMINGO, DiaSemana.LUNES, DiaSemana.MARTES, DiaSemana.MIERCOLES, DiaSemana.JUEVES, DiaSemana.VIERNES];
-
-//         for (const i in DIAS_A_GENERAR_TURNOS) {
-//             const fecha = new Date();
-//             fecha.setDate(now.getDate() + i); // Avanza la fecha en `i` días. Fuente: https://stackoverflow.com/a/563442
-
-//             const horariosDisponibles = medico.disponibilidades().filter((disponibilidad) => disponibilidad.diaSemana() === nombreDeDias[fecha.getDay()]);
-//             horariosDisponibles.forEach((horario) => {
-//                 fecha.setHours(horario.horaDesde(), 0, 0, 0);
-
-//                 // TODO Consultar en la base de datos si el turno en esa fecha ya existe
-
-//                 turnos.push(objetivo.generarTurno(medico, fecha));
-//             });
-//         }
-//         return turnos;
-//     }
