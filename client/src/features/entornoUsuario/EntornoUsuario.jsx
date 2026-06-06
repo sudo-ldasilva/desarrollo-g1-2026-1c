cf13ce27640b9201e4b8744ea2fb6afac2be9b5f
sudo-ldasilva <205469926+sudo-ldasilva@users.noreply.github.com>
2026-06-06 14:26:05 -0300

    Introduce al concepto de Entorno Usuario

---
+++ client/src/features/entornoUsuario/EntornoUsuario.jsx
@@ -0,0 +1,16 @@
+import Dashboard from "../dashboard/Dashboard.jsx"
+import MisTurnos from '../MisTurnos/MisTurnos.jsx';
+
+import './EntornoUsuario.css';
+
+const EntornoUsuario = () => {
+    return (
+        <>
+            <p>EntornoUsuario</p>
+            <Dashboard />
+            <MisTurnos />
+        </>
+    )
+};
+
+export default EntornoUsuario;
