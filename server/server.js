e54bbbb1f06e9850823a1ac0d12f79aed8923ac0
Julian <jcarpintero@frba.utn.edu.ar>
2026-06-06 09:50:58 -0300

    Llevé .env y .mongo-data al server, cambié cosas mínimas en server.js --> ejecuto desde tp-grupal/server> node server.js y funciona todo correcto

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ import app from "./app.js";
 import dotenv from "dotenv";
 import { connectDB, disconnectDB } from "./config/db.js";
 
+// --- NUEVOS IMPORTS PARA LAS RUTAS ---
+import path from "path";
+import { fileURLToPath } from "url";
+
 import "./models/UsuarioModel.js";
 import "./models/EspecialidadModel.js";
 import "./models/SedeModel.js";
@@ -12,8 +16,15 @@ import "./models/MedicoModel.js";
 import "./models/PracticaModel.js";
 // import "./DisponibilidadHorariaModel.js"; se descomenta cuando esten andando
 
-dotenv.config();
-const port = process.env.PORT;
+// --- CONFIGURACIÓN DE RUTAS COMPATIBLE CON ES MODULES ---
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Forzamos a dotenv a buscar el .env exactamente en la carpeta donde está este server.js
+dotenv.config({ path: path.resolve(__dirname, "./.env") });
+
+// Si process.env.PORT no viene (por si falla algo más), le dejamos el 3000 por defecto para que no explote
+const port = process.env.PORT || 3000;
 
 connectDB().then(() => {
     app.listen(port, () => {
