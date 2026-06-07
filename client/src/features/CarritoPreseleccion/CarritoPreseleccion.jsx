e1f3a8943fdfef28ef1f30d5cd86ed7983fcdfa6
Julian <jcarpintero@frba.utn.edu.ar>
2026-06-07 18:44:48 -0300

    Planteo estructura para pantalla SolicitarTurnos, voy a trabajar primero sobre la pantalla CarritoPreseleccion que se va a poder ver unicamente de la pantalla SolicitarTurnos

---
+++ client/src/features/CarritoPreseleccion/CarritoPreseleccion.jsx
@@ -0,0 +1,1 @@
+import './CarritoPreseleccion.css';
---
+++ client/src/features/SolicitarTurnos/SolicitarTurnos.jsx
@@ -0,0 +1,7 @@
+import './SolicitarTurnos.css';
+
+//Primero debo permitir todos los filtros necesarios y luego mostrar los turnos en estado DISPONIBLE
+// Debería utilizar el componente de calendarioMensualTurnos para allí poder ver los turnos disponibles
+//   y poder sacar el turno.
+
+// Cuando entrás a la pantalla de solicitar turnos debería aparecer en el navbar header el carrito de preselección
