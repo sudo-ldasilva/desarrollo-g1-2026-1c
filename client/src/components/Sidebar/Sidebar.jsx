3ba9f2a15769e9cf00862d48559b64e371448f62
ValentinOcchiuzzi <vocchiuzzi@frba.utn.edu.ar>
2026-06-18 16:57:08 -0300

    agrega iconos en sidebar

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -2,6 +2,13 @@ import { useMemo } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { Badge, IconButton, Box, Typography, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 // import NotificationsIcon from '@mui/icons-material/Notifications';
+import HomeIcon from '@mui/icons-material/Home';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import ScheduleIcon from '@mui/icons-material/Schedule';
+import MedicalInformationIcon from '@mui/icons-material/MedicalInformation';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 import { useAuth } from '../../hooks/useAuth.jsx';
 import './Sidebar.css';
 
@@ -17,13 +24,13 @@ const Sidebar = () => {
   const { userRole } = useAuth();
 
   const allMenuItems = [
-    { text: 'Inicio', path: '/app/dashboard', role: 'patient' },
-    { text: 'Mis Turnos', path: '/app/mis-turnos', role: 'patient' },
-    { text: 'Mi Agenda', path: '/app/mi-agenda', role: 'doctor' },
-    { text: 'Solicitar turnos', path: '/app/solicitar-turnos', role: 'patient' },
-    { text: 'Mis disponibilidades', path: '/app/mis-disponibilidades', role: 'doctor' },
-    { text: 'Mis servicios', path: '/app/mis-servicios', role: 'doctor' },
-    { text: 'Mis sedes', path: '/app/mis-sedes', role: 'doctor' },
+    { text: 'Inicio', icon: <HomeIcon />, path: '/app/dashboard', role: 'patient' },
+    { text: 'Mis Turnos', icon: <FavoriteIcon />, path: '/app/mis-turnos', role: 'patient' },
+    { text: 'Mi Agenda', icon: <CalendarMonthIcon />, path: '/app/mi-agenda', role: 'doctor' },
+    { text: 'Solicitar turnos', icon: <MedicalServicesIcon />, path: '/app/solicitar-turnos', role: 'patient' },
+    { text: 'Mis disponibilidades', icon: <ScheduleIcon />, path: '/app/mis-disponibilidades', role: 'doctor' },
+    { text: 'Mis servicios', icon: <MedicalInformationIcon />, path: '/app/mis-servicios', role: 'doctor' },
+    { text: 'Mis sedes', icon: <LocationOnIcon />, path: '/app/mis-sedes', role: 'doctor' },
   ];
 
   const menuItems = useMemo(() => {
@@ -55,6 +62,9 @@ const Sidebar = () => {
               onClick={() => handleNavigation(item.path)}
               className={`sidebar-menu-item ${location.pathname === item.path ? 'active' : ''}`}
             >
+              <ListItemIcon className="sidebar-menu-icon">
+                {item.icon}
+              </ListItemIcon>
               <ListItemText
                 primary={
                   <Box className="menu-item-text">
