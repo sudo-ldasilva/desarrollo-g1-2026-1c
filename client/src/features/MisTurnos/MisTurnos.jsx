3266ba59f1f55c335fa3a8fdd9190b244dbc3616
sudo-ldasilva <205469926+sudo-ldasilva@users.noreply.github.com>
2026-06-27 19:15:01 -0300

    Implementa funcionalidad de Mis Turnos

--- client/src/features/MisTurnos/MisTurnos.jsx
+++ client/src/features/MisTurnos/MisTurnos.jsx
@@ -1,11 +1,39 @@
 import { Card, CardContent, Box } from '@mui/material';
-import HistorialTurnos from '../../components/HistorialTurnos/HistorialTurnos';
+import {useState, useEffect, useRef} from 'react'
+import CardTurno from '../../components/CardTurno/CardTurno';
 import './MisTurnos.css';
+import { useAuth } from "../../hooks/useAuth.jsx";
+import {getTurnos} from "../../services/TurnosService.jsx"
 
-const MisTurnos = ({turnos}) => {
+const MisTurnos = () => {
+    const { isAuthenticated, getAccessToken } = useAuth();
+
+    const [turnos, setTurnos] = useState([])
+
+    useEffect( () => {
+        const descargarBD = async () => {
+            const accessToken = await getAccessToken(
+              process.env.REACT_APP_LOGTO_RESOURCES
+            );
+
+            const turnosDescargados = await getTurnos(accessToken)
+            setTurnos(turnosDescargados)
+        }
+
+        descargarBD();
+    }, [getAccessToken])
+
     return (
-        <div>
-            <HistorialTurnos turnos={turnos} />
+        <div className="historial-container">
+            {
+                turnos.length === 0 ? (
+                    <p className="listado-vacio">No tenés un historial de turnos activos.</p>
+                ) : (
+                    turnos.map((turno) => (
+                        <CardTurno key={turno._id || turno.id} turno={turno} />
+                    ))
+                )
+            }
         </div>
     );
 };
